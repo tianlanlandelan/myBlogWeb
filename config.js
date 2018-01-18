b4f737dfac486c064239d10174ad32516c30def1
yangkaile <guyexing@foxmail.com>
2018-01-18 22:44:47 +0800

    初始化导入

---
+++ config.js
@@ -0,0 +1,17 @@
+/**
+ * 配置文件，该文件描述后台接口地址等配置
+ */
+/**
+ * 主配置
+ */
+var myConfig = {
+        "hostUrl":"http://127.0.0.1:8080"
+    };
+/**
+ * 请求接口
+ */
+var myRequestUrl ={
+        "articleInfo":{
+            "getAll":myConfig.hostUrl + "/articleInfo/getAll"
+        }
+    };
