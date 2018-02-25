456d15ee2b871fe7ad759192c79a407e67551ab1
yangkaile <guyexing@foxmail.com>
2018-02-25 20:35:38 +0800

    添加顶部导航

--- js/my-vue.js
+++ js/my-vue.js
@@ -31,6 +31,33 @@ Vue.component('my-test',{
     }
 });
 
+Vue.component('my-top-menu',{
+    props:[],
+    template:'<el-row style="color: #fff;background-color: #545c66;padding: 10px;"> '+
+            ' <el-col :md="18"> '+
+            '     后端管理  '+
+            ' </el-col> '+
+            ' <el-col :md="2">  '+
+            '     <el-badge :value="12" class="item"> <el-button size="small">评论</el-button > </el-badge>   '+
+            ' </el-col> '+
+            ' <el-col :md="2">  '+
+            '     <el-badge :value="3" class="item"> <el-button size="small">回复</el-button>  </el-badge> '+
+            ' </el-col> '+
+            ' <el-col :md="2" > '+
+            '     <el-popover ref="popover1" placement="bottom" width="100" trigger="hover" > '+
+            '         <div><i class="el-icon-tickets"></i> My Profile</div> '+
+            '         <div><i class="el-icon-circle-close-outline"></i> Log Out</div>   '+
+            '     </el-popover> '+
+            '     <el-button v-popover:popover1 size="small">马面</el-button> '+
+            ' </el-col>     '+
+            ' </el-row>',
+    data: function () {
+        return {
+
+        }
+    }
+});
+
 /**
  * 左侧导航组件
  * 引用方式:<my-left-menu :left-menu-value="leftMenuValue"></my-left-menu>
