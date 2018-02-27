304a9e4a2be0f1266724dd731bb380cde4fd3016
yangkaile <guyexing@foxmail.com>
2018-02-27 11:00:04 +0800

    将vue组件模板修改为单文件组件

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,63 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+
+    <!-- <img src="./assets/logo.png">
     <div>
       <el-button @click="startHacking">Start</el-button>
-    </div>
-    <div>
-      <MyTopMenu></MyTopMenu>
-    </div>
+    </div> -->
+    <el-row>
+      <el-col :md="24">
+        <MyTopMenu></MyTopMenu>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :md="3">
+        <MyLeftMenu :left-menu-value="leftMenuValue"></MyLeftMenu>
+      </el-col>
+    </el-row>
+
   </div>
 </template>
 
 <script>
+var leftMenuValue = [
+    {"name":"内容管理","value":"0","index":0,"children":[
+        {"name":"文章管理","value":"1","url":"http://www.baidu.com"
+        },
+        {"name":"评论管理","value":"2","url":"http://www.baidu.com"
+        },
+        {"name":"分类管理","value":"3","url":"http://www.baidu.com"
+        },
+        {"name":"标签管理","value":"4","url":"http://www.baidu.com"
+        }
+    ]},
+    {"name":"用户管理","value":"10","index":0,"children":[
+        {"name":"用户列表","value":"11","url":"http://www.baidu.com"
+        }
+    ]},
+    {"name":"数据统计","value":"20","index":0,"children":[
+        {"name":"访问量","value":"21","url":"http://www.baidu.com"
+        }
+    ]},
+    {"name":"博客设置","value":"30","index":0,"children":[
+        {"name":"博客设置","value":"31","url":"http://www.baidu.com"
+        }
+    ]}
+    ];
+
 import MyTopMenu from './pages/MyTopMenu.vue'
+import MyLeftMenu from './pages/MyLeftMenu.vue'
+
 
 export default {
+  data:function(){
+    return{
+        leftMenuValue:leftMenuValue
+    }
+  },
   components:{
-    MyTopMenu
+    MyTopMenu,
+    MyLeftMenu
   },
   methods: {
     startHacking () {
---
+++ src/pages/MyLeftMenu.vue
@@ -0,0 +1,43 @@
+<template>
+    <el-menu    class="el-menu-vertical-demo" 
+                @open="handleOpen" 
+                @close="handleClose" 
+                background-color="#545c64" 
+                text-color="#fff" 
+                active-text-color="#ffd04b">
+        <el-submenu v-for="fatherMenu in leftMenuValue" :key="fatherMenu.name" v-bind:index="fatherMenu.value" >    
+            <template slot="title">   
+                <i class="el-icon-location"></i>  
+                <span>{{fatherMenu.name}} </span>    
+            </template>   
+            <el-menu-item   v-for="menu in fatherMenu.children" :key="menu.name" 
+                            style="min-width: 50px" 
+                            v-bind:index="menu.value" 
+                            @click="handleClick(menu.value,menu.name,menu.url)">
+                {{menu.name}}
+            </el-menu-item> 
+        </el-submenu> 
+    </el-menu>
+</template>
+<script>
+export default {
+    data: function () {
+        return {
+           
+        }
+    },
+    props:['leftMenuValue'],
+    methods:{
+        handleOpen:function(key, keyPath) {
+            console.log("handleOpen--",key, keyPath);
+        },
+        handleClose:function(key, keyPath) {
+            console.log("handleClose--",key, keyPath);
+        },
+        handleClick:function(a,b,c) {
+            console.log("handleClick--",a,b,c);
+        }
+    }
+}
+</script>
+    
